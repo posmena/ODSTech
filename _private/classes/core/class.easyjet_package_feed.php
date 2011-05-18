ce855cb827ed37c7d2f013ac2f8c8e05e18c65c0
Bobby Jason <me@bobbyjason.co.uk>
2011-05-18 21:51:15 +0100

    added new fields

--- _private/classes/core/class.easyjet_package_feed.php
+++ _private/classes/core/class.easyjet_package_feed.php
@@ -5,6 +5,8 @@ class core_easyjet_package_feed
 	private $template = 'n/a';
 	private $assignments;
 	private $file = '/Users/bobbeh/Sites/techodst-dev/_private/files/hostedfeeds/easyjet/packages.csv';
+	//private $file = '/var/www/techodst-dev/_private/files/hostedfeeds/easyjet/packages.csv';
+	
 	
 	public function __construct($db, $qs) {
 		if (array_key_exists('generate', $qs)) {
@@ -26,9 +28,32 @@ class core_easyjet_package_feed
 	public function generateFeed($db)
 	{
 		echo 'Generating Feed...';
-		$sql = "SELECT 
+		$sql = "SELECT raw.hotel_name, 
 		CONCAT('http://holidays.easyjet.com/dl.aspx?mode=FlightPlusHotel&depdate=' , DAY(raw.departure_date) , '/' , MONTH(raw.departure_date) , '/', YEAR(raw.departure_date) , '&nights=', raw.duration, '&adults=2&airport=', out_departure_airport_code, '&resort=', p.ResortID, '&property=', raw.property_id) as 'url',
-		p.*, raw.*  FROM pm_custom_products_easyjet raw
+		raw.country,
+		raw.region,
+		raw.resort,
+		raw.duration,
+		raw.board,
+		raw.cost,
+		raw.currency,
+		raw.departure_date,
+		raw.package_url,
+		raw.rating,
+		raw.property_id,
+		raw.image_url, 
+		raw.room_type,
+		raw.out_departure_airport_code,
+		raw.out_departure_airport_name,
+		raw.out_flight_departure_date,
+		raw.out_destination_airport_name,
+		raw.out_destination_airport_code,
+		raw.ret_departure_airport_code,
+		raw.ret_departure_airport_name,
+		raw.ret_flight_departure_date,
+		raw.ret_destination_airport_name,
+		raw.ret_destination_airport_code
+		FROM pm_custom_products_easyjet raw
 		INNER JOIN pm_scrape_property_lookup lookup ON lookup.scrape_id=raw.property_id
 		INNER JOIN Property p ON p.PropertyID=lookup.odst_id
 		LIMIT 0,10";
@@ -37,35 +62,55 @@ class core_easyjet_package_feed
 		$output = '';
 		$separator = '","';
 		$newline = "\r\n";
-		$header = '"name'. $separator .
+		$header = '"hotel_name'. $separator .
 					'url' . $separator .
-					'resort' . $separator .
-					'resort_id' . $separator .
-					'region' . $separator .
 					'country' . $separator .
+					'region' . $separator .
+					'resort' . $separator .
+					'duration' . $separator .
+					'board' . $separator .
+					'cost' . $separator .
+					'currency' . $separator .
+					'departure_date' . $separator .
+					'package_url' . $separator .
 					'rating' . $separator .
-					'hotel_type' . $separator .
-					'airport_code' . $separator .
-					'description' . $separator .
-					'address' . $separator .
-					'property_id' . $separator .
-					'image1_url' . $separator .
-					'postcode"' . $newline;
+					'image_url' . $separator .
+					'room_type' . $separator .
+					'out_departure_airport_code' . $separator .
+					'out_departure_airport_name' . $separator .
+					'out_flight_departure_date' . $separator .
+					'out_destination_airport_name' . $separator .
+					'out_destination_airport_code' . $separator .
+					'ret_departure_airport_code' . $separator .
+					'ret_departure_airport_name' . $separator .
+					'ret_flight_departure_date' . $separator .
+					'ret_destination_airport_name' . $separator .
+					'ret_destination_airport_code"' . $newline;
 		foreach($result as $property) {
-			$output .= '"'.$property['Name'] . $separator .
+			$output .= '"'.$property['hotel_name'] . $separator .
 							$property['url'] . $separator .
-							$property['Resort'] . $separator .
-							$property['ResortID'] . $separator .
-							$property['Region'] . $separator .
-							$property['Country'] . $separator .
-							$property['Rating'] . $separator .
-							$property['HotelType'] . $separator .
-							$property['AirportCode'] . $separator .
-							$property['Description'] . $separator .
-							$property['Address'] . $separator .
-							$property['PropertyID'] . $separator .
-							$property['Image1URL'] . $separator .
-							$property['PostCode'] . '"'.$newline;
+							$property['country'] . $separator .
+							$property['region'] . $separator .
+							$property['resort'] . $separator .
+							$property['duration'] . $separator .
+							$property['board'] . $separator .
+							$property['cost'] . $separator .
+							$property['currency'] . $separator .
+							$property['departure_date'] . $separator .
+							$property['package_url'] . $separator .
+							$property['rating'] . $separator .
+							$property['image_url'] . $separator .
+							$property['room_type'] . $separator .
+							$property['out_departure_airport_code'] . $separator .
+							$property['out_departure_airport_name'] . $separator .
+							$property['out_flight_departure_date'] . $separator .
+							$property['out_destination_airport_name'] . $separator .
+							$property['out_destination_airport_code'] . $separator .
+							$property['ret_departure_airport_code'] . $separator .
+							$property['ret_departure_airport_name'] . $separator .
+							$property['ret_flight_departure_date'] . $separator .
+							$property['ret_destination_airport_name'] . $separator .
+							$property['ret_destination_airport_code'] . '"'.$newline;
 		}
 		
 		$output = $header . $output;
