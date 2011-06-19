82d387191309248d70b8f546a99f9a87c1251f2b
Bobby Jason <me@bobbyjason.co.uk>
2011-06-19 22:08:53 +0100

    changed zipper

--- _private/zipFiles.sh
+++ _private/zipFiles.sh
@@ -1,8 +1,4 @@
-#for i in files/hostedfeeds/easyjet/*; do 
-#  zip -r "compressed/$i.zip" "$i"; 
-#done
-
 for i in $(ls -a files/hostedfeeds/easyjet | grep '.csv' | grep -v '.zip'); do
-   zip -r "files/compressedfeeds/easyjet/$i.zip" "files/hostedfeeds/easyjet/$i"
+   zip -j "files/compressedfeeds/easyjet/$i.zip" "files/hostedfeeds/easyjet/$i"
    rm "files/hostedfeeds/easyjet/$i"
 done
