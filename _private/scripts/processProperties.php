7480c57889c319b9ad6b01006bded01928b37cde
Bobby Jason <me@bobbyjason.co.uk>
2011-06-28 23:00:06 +0100

    added property url to feed

--- _private/scripts/processProperties.php
+++ _private/scripts/processProperties.php
@@ -25,6 +25,7 @@ switch($argv[1]) {
 		foreach($prices as $price) {
 			$priceProp = $properties[$price['_id']];
 			$priceProp['price'] = $price['price'];
+			$priceProp['url'] = 'http://holidays.easyjet.com/Holiday.aspx?PropertyID=' . $priceProp['propertyid'];
 			$collection->save($priceProp);
 		}
 		echo $collection->find()->count(). ' properties inserted';
