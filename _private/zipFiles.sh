ccb5a634bd25b0d3ad82c18cf576e22b3e40e67f
Bobby Jason <me@bobbyjason.co.uk>
2011-07-26 22:21:24 +0100

    added zipper

--- _private/zipFiles.sh
+++ _private/zipFiles.sh
@@ -2,3 +2,8 @@ for i in $(ls -a files/hostedfeeds/easyjet | grep '.csv' | grep -v '.zip'); do
    zip -j "files/compressedfeeds/easyjet/$i.zip" "files/hostedfeeds/easyjet/$i"
    rm "files/hostedfeeds/easyjet/$i"
 done
+
+for i in $(ls -a files/hostedfeeds/jtspas | grep '.csv' | grep -v '.zip'); do
+   zip -j "files/compressedfeeds/jtspas/$i.zip" "files/hostedfeeds/jtspas/$i"
+   rm "files/hostedfeeds/jtspas/$i"
+done
