727bb5c2ae0471607a2ee657d241348411964ed0
Bobby Jason <me@bobbyjason.co.uk>
2011-09-28 20:45:14 +0100

    changed for google

--- _private/classes/feed_processing/class.xmlgenerator.php
+++ _private/classes/feed_processing/class.xmlgenerator.php
@@ -18,7 +18,12 @@ class ODSTech_xmlgenerator
 		$comma      = ",";
 		$i          = 0;
 		$xmlDoc     = '<?xml version="1.0" encoding="UTF-8" ?>';
-		$xmlContent = '<products>';
+		$xmlContent = '<rss version ="2.0" xmlns:g="http://base.google.com/ns/1.0">';
+		$xmlContent .= '<channel>';
+
+		$xmlContent .= '<title>'. $name . ' Datafeed</title>';
+		$xmlContent .= '<description>List of all our products.</description>';
+		$xmlContent .= '<link>http://www.odst.co.uk/'.$name . '</link>';
 
 		while ($data = fgetcsv($handle, null, $comma)) {
 			$i++;
@@ -28,7 +33,7 @@ class ODSTech_xmlgenerator
 				continue;
 			}
 			
-			$xmlContent .= '<product>';
+			$xmlContent .= '<item>';
 			foreach ($headers as $key => $heading) {
 				if ($heading == 'product_type' || $heading == 'description' || $heading == 'title' || $heading == 'delivery_time') {
 					$content = htmlentities($data[$key]);
@@ -36,12 +41,13 @@ class ODSTech_xmlgenerator
 					$content = $data[$key];
 				}
 				
-				$xmlContent .= '<g:' . $heading . '>' . $content . '</' . $heading . '>' . "\n";	
+				$xmlContent .= '<g:' . $heading . '>' . $content . '</g:' . $heading . '>' . "\n";	
 				
 			}
-			$xmlContent .= '</product>';
+			$xmlContent .= '</item>';
 		}
-		$xmlContent .= '</products>';
+		$xmlContent .= '</channel>';
+		$xmlContent .= '</rss>';
 		
 		$xmlDoc .= $xmlContent;
 		fclose($handle);
