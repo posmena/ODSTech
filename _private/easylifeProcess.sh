86498764873ecdd39dc88e758c209a5e14270b8e
Bobby Jason <me@bobbyjason.co.uk>
2011-10-29 12:46:22 +0100

    easylife process

--- _private/easylifeProcess.sh
+++ _private/easylifeProcess.sh
@@ -6,7 +6,7 @@ cd /var/www/odst-live/_private
 
 php scripts/processFeed.php easylife | mail -s 'EasyLife Processed' tech@odst.co.uk
 
-echo 'http://www.odst.co.uk/feeds/easylife.zip?type=afuture' > logs/jtspatype.log
+echo 'http://www.odst.co.uk/feeds/easylife.zip?type=afuture' > logs/easylife.log
 mongoexport -d odstech -c live_easylife --csv -f 'productid','productname','deeplink','brand','price','image_link','category','availability','manufacturer','description' -o files/hostedfeeds/easylife/afuture.csv | mail -s "Easylife Afuture Exported" tech@odst.co.uk < logs/easylife.log
 
 
