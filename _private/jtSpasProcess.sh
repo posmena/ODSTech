b12981680cba562f1fce42d741d9f25fe22b9de7
Bobby Jason <me@bobbyjason.co.uk>
2011-11-07 19:19:30 +0000

    changes for jtSpas

--- _private/jtSpasProcess.sh
+++ _private/jtSpasProcess.sh
@@ -20,10 +20,10 @@ echo 'http://www.odst.co.uk/feeds/jtspas.zip?type=nextag' > logs/jtspatype.log
 mongoexport -d odstech -c live_jtspas --csv -f 'title','mpn','id','description','price','deeplink','image_link','nextag_category','category','availability','condition','shipping','shipping_weight','shipping_uk','shipping_cost_uk','gtin','brand','nextag_deeplink','rrp' -o files/hostedfeeds/jtspas/nextag.csv | mail -s "Nextag JtSpas Exported" tech@odst.co.uk < logs/jtspatype.log
 
 echo 'http://www.odst.co.uk/feeds/jtspas.zip?type=dooyoo' > logs/jtspatype.log
-mongoexport -d odstech -c jtspas --csv -f 'category','id','title','price','deeplink','description','image_link','warranty','shipping','shipping_weight','shipping_uk','shipping_cost_uk','availability','condition','gtin','brand','dooyoo_deeplink','rrp' -o files/hostedfeeds/jtspas/dooyoo.csv | mail -s "Dooyoo JtSpas Exported" tech@odst.co.uk < logs/jtspatype.log
+mongoexport -d odstech -c live_jtspas --csv -f 'category','id','title','price','deeplink','description','image_link','warranty','shipping','shipping_weight','shipping_uk','shipping_cost_uk','availability','condition','gtin','brand','dooyoo_deeplink','rrp' -o files/hostedfeeds/jtspas/dooyoo.csv | mail -s "Dooyoo JtSpas Exported" tech@odst.co.uk < logs/jtspatype.log
 
 echo 'http://www.odst.co.uk/feeds/jtspas.zip?type=bing' > logs/jtspatype.log
-mongoexport -d odstech -c jtspas --csv -f 'id','title','price','description','condition','shipping','shipping_weight','shipping_uk','shipping_cost_uk','brand','mpn','image_link','category','quantity','availability','expiration_date','deeplink','rrp' -o files/hostedfeeds/jtspas/bing.csv | mail -s "Bing JtSpas Exported" tech@odst.co.uk < logs/jtspatype.log
+mongoexport -d odstech -c live_jtspas --csv -f 'id','title','price','description','condition','shipping','shipping_weight','shipping_uk','shipping_cost_uk','brand','mpn','image_link','category','quantity','availability','expiration_date','deeplink','rrp' -o files/hostedfeeds/jtspas/bing.csv | mail -s "Bing JtSpas Exported" tech@odst.co.uk < logs/jtspatype.log
 
 ./zipFiles.sh
 
