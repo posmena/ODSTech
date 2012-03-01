828b013fc1790e1bc6812c1c62894aca93f22ecc
Mike Hinchcliffe <mike@odst.co.uk>
2012-03-01 22:28:36 +0800

    Easy life feed update

--- _private/easylifeProcess.sh
+++ _private/easylifeProcess.sh
@@ -7,7 +7,10 @@ cd /var/www/odst-live/_private
 php scripts/processFeed.php easylife | mail -s 'EasyLife Processed' tech@odst.co.uk
 
 echo 'http://www.odst.co.uk/feeds/easylife.zip?type=afuture' > logs/easylife.log
-mongoexport -d odstech -c live_easylife --csv -f 'productid','productname','deeplink','brand','price','image_link','category','availability','manufacturer','description' -o files/hostedfeeds/easylife/afuture.csv | mail -s "Easylife Afuture Exported" tech@odst.co.uk < logs/easylife.log
+mongoexport -d odstech -c ot_easylife --csv -f 'productid','productname','deeplink','brand','price','image_link','image_link2','category','availability','manufacturer','description' -o files/hostedfeeds/easylife/afuture.csv | mail -s "Easylife Afuture Exported" tech@odst.co.uk < logs/easylife.log
+
+echo 'http://www.odst.co.uk/feeds/easylife.zip?type=custom' > logs/easylife.log
+mongoexport -d odstech -c ot_easylife --csv -f 'id','title','deeplink','brand','price','image_link','image_link2','product_type','availability','description','condition','gtin' -o files/hostedfeeds/easylife/custom.csv | mail -s "Easylife Custom Exported" tech@odst.co.uk < logs/easylife.log
 
 
 ./zipFiles.sh
