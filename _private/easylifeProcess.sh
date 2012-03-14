bed56117525fe21d47803e7a2a20c41d9860ace3
Mike Hinchcliffe <mike@odst.co.uk>
2012-03-14 14:36:48 +0800

    Remove UTF8 chars for excel

--- _private/easylifeProcess.sh
+++ _private/easylifeProcess.sh
@@ -11,8 +11,11 @@ php scripts/scraper.php easylife
 echo 'http://www.odst.co.uk/feeds/easylife.zip?type=afuture' > logs/easylife.log
 mongoexport -d odstech -c ot_easylife --csv -f 'productid','productname','deeplink','brand','price','image_link','image_link2','category','availability','manufacturer','description' -o files/hostedfeeds/easylife/afuture.csv | mail -s "Easylife Afuture Exported" tech@odst.co.uk < logs/easylife.log
 
+iconv -c -f utf-8 -t ascii files/hostedfeeds/easylife/afuture.csv
+
 echo 'http://www.odst.co.uk/feeds/easylife.zip?type=custom' > logs/easylife.log
 mongoexport -d odstech -c ot_easylife --csv -f 'id','title','deeplink','brand','price','image_link','image_link2','image_link3','product_type','availability','description','condition','gtin' -o files/hostedfeeds/easylife/custom.csv | mail -s "Easylife Custom Exported" tech@odst.co.uk < logs/easylife.log
 
+iconv -c -f utf-8 -t ascii files/hostedfeeds/easylife/custom.csv
 
 ./zipFiles.sh
