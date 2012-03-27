879218835441801fd41437f965943abe78f9290a
Mike Hinchcliffe <mike@odst.co.uk>
2012-03-27 17:41:36 +0100

    Style Piques Scraper

--- _private/classes/core/class.stylepiques_feed.php
+++ _private/classes/core/class.stylepiques_feed.php
@@ -1,6 +1,6 @@
 <?php
 
-class core_damsel_feed extends core_feed_downloader
+class core_stylepiques_feed extends core_feed_downloader
 {
 	public function __construct($db, $qs) {
 		parent::__construct($db, $qs, 'stylepiques');
