4c49089f0f9d9a405e386a31052d769a7a296216
Mike Hinchcliffe <mike@odst.co.uk>
2012-05-19 16:35:29 +0100

    fix

--- _private/classes/core/class.document_downloader.php
+++ _private/classes/core/class.document_downloader.php
@@ -6,40 +6,13 @@ class core_document_downloader
 	private $assignments;
 	private $file = '';
 	private $isHome = false;
-	private $feedName = 'blank';
+	private $documentName = 'blank';
 	
-	public function __construct($db, $qs, $feedName = 'blank') {
+	public function __construct($db, $qs, $documentName = 'blank') {
 		
-		$this->feedName = $feedName;
-		if (false === array_key_exists('type', $qs)) {
-			print 'Type must be supplied';
-			exit;
-		}
-
-		if (false === array_key_exists('format', $qs)) {
-			$format = 'csv';
-		} else {
-			$format = $qs['format'];
-		}
-
-		switch ($format) {
-			case 'csv':
-			{
-				$ext = '.csv.zip';
-				$this->file = configuration::APPROOT . '_private/files/compressedfeeds/'. $this->feedName .'/'.strtolower($qs['type']) . '.csv.zip';
-
-				if (isset($qs['unzipped'])){
-					$ext = '.csv';
-					$expandedPath = configuration::APPROOT . '_private/files/inflated/' . $feedName . '/';
-
-					if (false === file_exists($this->file)) {
-						throw new Exception ('File: ' . $filepath . ' does not exist');
-					}
-
-					shell_exec('unzip -jo ' . $this->file . ' -d ' . $expandedPath);
-
-					$this->file = $expandedPath . '/' . strtolower($qs['type']) . '.csv';
-				}
+		$this->documentName = $documentName;
+		
+				$this->file = configuration::APPROOT . '_private/files/downloads/'. $this->documentName;
 
 				if (true === file_exists($this->file)) {
 					header("Content-type: application/octet-stream");
@@ -52,37 +25,8 @@ class core_document_downloader
 					print 'File does not exist';
 					exit;
 				}	
-
-				break;
-			}
-			case 'froogle':
-			case 'xml':
-			{
-				$this->file = configuration::APPROOT . '_private/files/hostedfeeds/' . $this->feedName. '/'.strtolower($qs['type']) . '.xml';
-
-				if (true === file_exists($this->file)) {
-					header("Content-type: application/octet-stream");
-					header("Content-Disposition: attachment; filename=\"" . $this->feedName . "-" .$qs['type']. ".xml\"");
-					header("Content-type: application/force-download"); 
-				    //header("Content-length: ".filesize($this->file)); 
-				    readfile($this->file);
-					exit; 
-				} else {
-					print 'File does not exist';
-					exit;
-				}	
-
-				break;
-				
-			}
-			default:
-			{
-				print 'Invalid format';
-				exit;
-			}
-
 		}
-	}
+	
 	
 	public function getTemplate() {
 		return $this->template;
