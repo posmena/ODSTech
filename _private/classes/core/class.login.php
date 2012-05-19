11dca643dcee210ee689ede2a554ef40d0f291c3
Mike Hinchcliffe <mike@odst.co.uk>
2012-05-19 17:05:05 +0100

    login

--- _private/classes/core/class.login.php
+++ _private/classes/core/class.login.php
@@ -18,17 +18,29 @@ class core_login extends core_default
 		}
 		*/
 		if (true === array_key_exists('login', $qs)) {
-			/*
+			
 			$username = $qs['email'];
 			$password = $qs['password'];
 			$user = new user($db, $username, $password);
 			if ($user->login() === false) {
-				*/
-				$this->assignments['page']['feedback'] = 'You credentials are not authorised to login here.';
+				
+				$this->assignments['page']['feedback'] = 'Incorrect user name or password.';
 				return false;
-
-			//}
+			else
+				{
+				$redirect = "/";
+				
+				if( isset($qs['url']) )
+					{
+					$redirect = $qs['url'];
+					}
+				}
+				
+				util::setSession('user', $user);
+				util::redirect($redirect);		
+			}
 			
+			// CLIENT STUFF?
 			if (true === ctype_digit($user->getClientId())) {
 				$redirect = '/clients/'.$user->getClientId().'/dashboard';
 			}
