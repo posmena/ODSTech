2fd3c7e2a545d897950efb6cd1e4bce75b359027
Mike Hinchcliffe <mike@odst.co.uk>
2012-07-18 16:58:33 +0100

    capitalise option name

--- _private/classes/core/p20/class.client.php
+++ _private/classes/core/p20/class.client.php
@@ -68,6 +68,9 @@ class core_p20_client extends core_default
 				{
 				if( $field['searchable'] == true )
 					{
+					$field['fieldname']  = preg_replace("/[^a-zA-Z0-9\s]/", " ", $field['fieldname']);
+					$field['fieldname'] = ucwords($field['fieldname']);
+					
 					$feed['searchoptions'][] = $field;
 					}
 				}
