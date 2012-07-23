cb16f6242b1d4dbaa47492e5d2c1293f1b0496ba
Mike Hinchcliffe <mike@odst.co.uk>
2012-07-23 14:01:48 +0100

    gift experience app

---
+++ public_html/facebook/apps/thegiftexperience/index.php
@@ -0,0 +1,32 @@
+<?php
+
+function parse_signed_request($signed_request, $secret) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+    error_log('Unknown algorithm. Expected HMAC-SHA256');
+    return null;
+  }
+
+  // check sig
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+
+
+$signed = parse_signed_request($_REQUEST['signed_request'], "dab79a06068c4600754fcae06e5b35f9");
+print_r($signed);
+?>
