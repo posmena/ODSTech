be03b8bd0ed9f23aa17f4e479a82e619d09eebbd
Mike Hinchcliffe <mike@odst.co.uk>
2012-07-23 15:07:38 +0100

    test

--- public_html/facebook/apps/thegiftexperience/index.php
+++ public_html/facebook/apps/thegiftexperience/index.php
@@ -25,11 +25,48 @@ function parse_signed_request($signed_request, $secret) {
 function base64_url_decode($input) {
   return base64_decode(strtr($input, '-_', '+/'));
 }
-print_r($_POST);
-print("<br><br>");
-print_r($_GET);
 
+$liked = false;
 
 $signed = parse_signed_request($_POST['signed_request'], "dab79a06068c4600754fcae06e5b35f9");
-print_r($signed);
+if( $signed && $signed['liked'] == 1 )
+	{
+	$liked = true;
+	}
 ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+$('#share_button').click(function(e){
+e.preventDefault();
+FB.ui(
+{
+method: 'feed',
+name: 'This is the content of the "name" field.',
+link: ' http://www.hyperarts.com/',
+picture: 'http://www.hyperarts.com/external-xfbml/share-image.gif',
+caption: 'This is the content of the "caption" field.',
+description: 'This is the content of the "description" field, below the caption.',
+message: ''
+});
+});
+});
+</script>
+
+
+
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+FB.init({appId: '461798647173464', status: true, cookie: true,
+xfbml: true});
+};
+(function() {
+var e = document.createElement('script'); e.async = true;
+e.src = document.location.protocol +
+'//connect.facebook.net/en_US/all.js';
+document.getElementById('fb-root').appendChild(e);
+}());
+</script>
