d30581660f677c7707da12927c928130772ee9c6
Mike Hinchcliffe <mike@odst.co.uk>
2012-07-24 18:22:18 +0100

    dynamically set page url

--- public_html/facebook/apps/thegiftexperience/index.php
+++ public_html/facebook/apps/thegiftexperience/index.php
@@ -27,6 +27,7 @@ function base64_url_decode($input) {
 }
 
 $liked = false;
+$url = "http://www.facebook.com/profile.php?id=". $signed['page']['id'] . "#v=app_461798647173464";
 
 if( isset($_POST['signed_request']) )
 	{
@@ -36,6 +37,8 @@ if( isset($_POST['signed_request']) )
 		$liked = true;
 		}
 	}
+	
+$url = "http://www.facebook.com/profile.php?id=". $signed['page']['id'] . "#v=app_461798647173464";
 ?>
 
 <html>
@@ -54,7 +57,7 @@ FB.ui(
 {
 method: 'feed',
 name: 'Special discount voucher for thegiftexperience.co.uk',
-link: 'http://www.facebook.com/pages/ODST_co_uk/116457125039404?sk=app_461798647173464',
+link: '<?php echo($url);?>',
 picture: 'http://www.odst.co.uk/facebook/apps/thegiftexperience/the_gift_experience.png',
 caption: 'I have just got my voucher!',
 description: 'You can claim yours by visiting the giftexperience.co.uk Facebook page.',
