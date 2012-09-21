ca868c4377a8947359524d16d5bcd4e6586edb07
Mike Hinchcliffe <mike@odst.co.uk>
2012-09-21 11:05:25 -0300

    check for blank price

--- _private/classes/feed_processing/class.goldenfeeds_feed.php
+++ _private/classes/feed_processing/class.goldenfeeds_feed.php
@@ -105,7 +105,10 @@ class goldenfeeds_feed extends network_base
 				}
 			else
 				{
-				$products->save($product);			
+				if( $product['price'] != "" )
+					{
+					$products->save($product);			
+					}
 				}
 		}
 		
