303338d60bb63e19b0533b62bdde46b99e3acf21
Mike Hinchcliffe <mike@odst.co.uk>
2012-10-23 20:05:39 +0100

    ssl

--- public_html/api/p20/p20.js
+++ public_html/api/p20/p20.js
@@ -200,8 +200,15 @@ BrowserDetect.proptype = {
 			var fileref=document.createElement("link")
 				  fileref.setAttribute("rel", "stylesheet")
 				  fileref.setAttribute("type", "text/css")
-				  fileref.setAttribute("href", "http://odst.co.uk/api/p20/css/odst_carousel.css")
-				  
+				  if( ssl )
+					{
+					fileref.setAttribute("href", "https://odst.co.uk/api/p20/css/odst_carousel.css")
+				    }
+				  else
+					{
+					fileref.setAttribute("href", "http://odst.co.uk/api/p20/css/odst_carousel.css")				  
+					}
+					
 			document.getElementsByTagName('head')[0].appendChild(fileref);
  
     },		
