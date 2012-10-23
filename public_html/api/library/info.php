88068f856fe4ae153185be1ca2f2dcfb2e0c49e3
Mike Hinchcliffe <mike@odst.co.uk>
2012-10-23 16:15:12 +0100

    library app

---
+++ public_html/api/library/info.php
@@ -0,0 +1,13 @@
+<html><body><b>Hackney Central Library</b><br/><br/>        		
+Hackney Technology and Learning Centre<br/>
+1 Reading Lane<br/>
+E8 1GQ<br/><br/><br/>
+<b>Opening Hours</b><br/><br/>
+Mon to Thurs 9am - 8pm<br/>
+Fri 9am - 6pm<br/>
+Sat 9am - 5pm<br/>
+Closed Sunday<br/><br/>
+Toy Library Tues: <br/>
+10.30am - 12pm<br/><br/>
+Under 5s Fri: <br/>11am - 12pm.<br/><br/>
+Tel:	020 8356 4358 <br/>(020 8356 2539 Renewals Line)</body></html> 
---
+++ public_html/api/library/whatson.php
@@ -0,0 +1,70 @@
+<html>
+<body>
+<h3>Black History Month - Surviving conflict, an exhibition by Joy Richardson </h3>
+<p><strong>Monday 1 October - Wednesday 31 October, during library opening hours</strong><br>
+In these series of oil paintings and drawings, themes of isolation and being part of a community are explored. People engage with the daily rituals of life, but the context in which these rituals are performed have a deep impact on a feeling of safety and finding a place in the world. The adults and children in the paintings often shy away from the viewer, denying eye contact or returning our gaze.<br>
+No booking required.</p>
+<h3>Black History Month - Midnight central reading group presents Octavia Butler </h3>
+<p><strong>Wednesday 31 October, 6pm - 7.30pm</strong> <br>
+Midnight central book group celebrates black writing and authors in all forms.&nbsp;In October we will&nbsp;look at the work of Octavia Butler. A stereotype-shattering science fiction writer,&nbsp;she was one of the first African American women to gain major critical acclaim in this genre. We will&nbsp;look at her major works and interviews and discussing Kindred in detail. Expect plenty of friendly discussion, meeting of minds and sharing your love of literature. Join us in honouring this exciting and timeless writer.<br>
+Reserve or collect your copy of Kindred at any Hackney Library. Refreshments will be served.&nbsp;<br>
+Age 16+, no booking required.</p>
+<h3>Family learning festival - IAG employability back to work confidence workshop</h3>
+<p><strong>Wednesday 24 October, 2pm - 4pm</strong><br>
+For adults who would like access to information, careers advice and resources to&nbsp;help you make more effective skills, work and life choices. Please note that adults who are accompanied by children must supervise them at all times.</p>
+<h3>Family learning festival - hand-sewn cushion making</h3>
+<p><strong>Thursday 25 October, 10.30am - 2.30pm<br>
+</strong>Design and make a beautiful cushion that you can use in your home or for a gift. Learn how to appliqué on fabric using hand-sewn stitches&nbsp;or create patchwork from reusable fabrics. You can bring in your own fabrics to personalise the cushion if you wish. Please bring in a cushion pad measuring 40cm x 40cm (16in x 16in) approximately. </p>
+<h3>Family learning festival&nbsp;- flamenco dance taster workshop</h3>
+<p><strong>Friday 26 October, 6.15pm - 7.45pm</strong> <br>
+Learn about flamenco and have a go. Professional tutor Gabriella will teach you some of the basics of flamenco dance. Come along and enjoy this fantastic workshop! <br>
+Age 5+ &nbsp;&nbsp;</p>
+<h3>Family learning festival - creative jewellery workshop</h3>
+<p><strong>Saturday 27 October, 2pm - 4pm</strong> <br>
+Come along for family fun creating your own piece of jewellery from different materials.<br>
+Age 7+ &nbsp;&nbsp;&nbsp;</p>
+<h3>Family learning festival - yummy food workshop</h3>
+<p><strong>Monday 29 October, 10.30am - 12noon</strong>&nbsp;&nbsp; <br>
+Be healthy and inventive and make your own delicious packed lunch to take away with you. Find out how to make a super nutritious sandwich and dessert.<br>
+Age 5+</p>
+<h3>Family learning festival - fun with numbers</h3>
+<p><strong>Thursday 1 November, 10am - 12noon</strong> <br>
+An entertaining and enjoyable workshop for all the family, designed to give you great ideas for making maths fun at home.&nbsp;Also&nbsp;an opportunity to discuss better ways to manage your family finances.</p>
+<h3>Family learning festival - hola! Spanish for beginners</h3>
+<p><strong>Friday 2 November, 2pm - 3.30pm</strong>&nbsp;<br>
+Would you like to learn Spanish? This is an opportunity to learn basic vocabulary and find out more about the language. Refreshments provided.&nbsp;Book in person at the library or call 0208 356 3000.<br>
+Age 7+&nbsp;&nbsp;&nbsp;&nbsp;</p>
+<p><strong>Regular&nbsp;events:</strong></p>
+<h3>Free community law shop sessions</h3>
+<p><strong>Every Monday, 2pm-5pm. No appointment neccessary<br>
+</strong>These free sessions let Hackney residents speak to qualified advisors about their housing, employment, immigration, welfare benefits and debt problems. The sessions are kindly sponsored by the council and Matrix chambers.</p>
+<h3><b>Teenage reading group</b></h3>
+<p>The next meeting of the&nbsp;is <strong>Wednesday 7 November</strong>, 6pm - 7.30pm and November's books are a selection from the shortlist for CILIP Carnegie medal.</p>
+<h3>Kids chess club </h3>
+<p><strong>Fortnightly on a Saturday 2pm-4pm<br>
+</strong>Chess is fascinating and the most popular strategy game in the world. Enthusiasm for chess is infectious. If you are aged between 7 and 11 years old and you&nbsp;like chess or would like to learn, come along to the Central library, try it out and see for yourself! All level players are welcome - from beginners to the more advanced. &nbsp;&nbsp;&nbsp;</p>
+<h3>Health checks with TLC Care Services and the Stroke project&nbsp;</h3>
+<p><strong>Fortnightly on a Wednesday 11am-4pm</strong><br>
+Everyone is at risk of developing heart disease, stroke, diabetes or kidney disease. The good news is that these conditions can often be prevented - even if you have a history of them in your family. Have your free NHS health check at these special drop-in sessions at the Central library and you will be better prepared for the future and be able to take steps to maintain or improve your health.</p>
+<h3>The ESOL carry on reading group</h3>
+<p><strong>First&nbsp;Thursday of every month 6pm-7.30 pm</strong><br>
+A group for English learners who want to improve their&nbsp;reading and speaking&nbsp;and meet new people, run by an ESOL qualified teacher. This is not a class, it is an informal group where we read and discuss books. For further information please contact Lisa or Ana at Hackney Central Library or phone on 020 8356 3000.</p>
+<h3><b>Coffee, Cake and Book group</b></h3>
+<p><strong>Monthly on a&nbsp;Wednesday, 10.30am-11.30am<br>
+</strong>Join this lively group at our next meeting on Wednesday 24 October when the group will be discussing W H Auden: Collected Poems selected by John Fuller. The group will also be reading Trespass by Rose Tremain.&nbsp;Reserve or collect a copy from any of the libraries. For further details contact <a href="mailto:lydia.julien@hackney.gov.uk">lydia.julien@hackney.gov.uk</a>.</p>
+<h3>The Reading (Lane) book group</h3>
+<p><strong>Every&nbsp;third Wednesday of the month 6.30pm-7.30pm<br>
+</strong>An informal and friendly book group for all lovers of literature. Why not join us at our next meeting on Wednesday 17 October when we'll be discussing&nbsp;There but for the by Ali Smith.&nbsp;Reserve or collect a copy from any of the libraries. Contact <a href="mailto:lydia.julien@hackney.gov.uk">lydia.julien@hackney.gov.uk</a> for further details.</p>
+<h3>Under 5s Story Session</h3>
+<p><strong>Fridays, 11am-12noon</strong><br>
+These sessions will not run during the summer holidays. Please contact the library for dates.</p>
+<h3>Toy Library </h3>
+<p><strong>Tuesdays, 10.30am-12.30pm</strong><br>
+A wide range of toys to play with and borrow. Age 5 and under - just turn up.</p>
+<h3>Older people's drop-in session&nbsp;&nbsp; </h3>
+<p><strong>First Thursday of the month, 2pm-4pm</strong><br>
+If you're over 55 why not come and have a cup of tea and a chat with us at our monthly drop-in session. To find out more about the session and any special events that we have planned call&nbsp;020&nbsp;8356 2550 and ask for Robert, or 020 8356 2547 and ask for Lydia.</p>
+<h3>Chatterbooks reading dragons - boys only group</h3>
+<p>The next meeting is Thursday 15 November, 4pm - 5pm and November's book is Captain Underpants And The Terrifying Return of Tippy Tinkletrousers by Dav Pilkey.</p>
+</body>
+</html>
