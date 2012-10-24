edf919c9f4e50af15f6c48d5df434ecb8262656b
Mike Hinchcliffe <mike@odst.co.uk>
2012-10-24 19:24:39 +0100

    amazon class

---
+++ public_html/api/library/sampleSettings.php
@@ -0,0 +1,6 @@
+<?php
+define('AWS_API_KEY', 'AKIAI3UTKX2TOYT6CIFQ');
+define('AWS_API_SECRET_KEY', 'GUiFOcGlI3DQRLVWzAsZvlmbFKBxUt6hQKtosECH');
+define('AWS_ASSOCIATE_TAG', 'inteenerltd-21');
+define('AWS_ANOTHER_ASSOCIATE_TAG', 'inteenerltd-21');
+
