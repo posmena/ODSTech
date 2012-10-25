f6d0be106dca3b0aad5ba54ddb50a9dc517bc2a4
Mike Hinchcliffe <mike@odst.co.uk>
2012-10-25 19:43:11 +0100

    blank if no results

--- public_html/api/library/search.php
+++ public_html/api/library/search.php
@@ -18,6 +18,7 @@ require 'AmazonECS.class.php';
 try
 {
     $amazonEcs = new AmazonECS(AWS_API_KEY, AWS_API_SECRET_KEY, 'co.uk', AWS_ASSOCIATE_TAG);
+	$books = null;
 
     // for the new version of the wsdl its required to provide a associate Tag
     // @see https://affiliate-program.amazon.com/gp/advertising/api/detail/api-changes.html?ie=UTF8&pf_rd_t=501&ref_=amb_link_83957571_2&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=&pf_rd_s=assoc-center-1&pf_rd_r=&pf_rd_i=assoc-api-detail-2-v2
@@ -31,6 +32,12 @@ try
 	//var_dump($response);
 	//die();
 	
+	if( !isset($response->Items->Item) )
+		{
+		echo "";
+		die();
+		}
+		
     $results = count($response->Items->Item);
 		
 	for( $i=0; $i < $results; $i++ )
