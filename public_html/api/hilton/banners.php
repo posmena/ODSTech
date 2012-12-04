26ee131ea1f625287d9c9a79f8a0d71c379d028f
Mike Hinchcliffe <mike@odst.co.uk>
2012-12-04 18:35:44 +0100

    fix

--- public_html/api/hilton/banners.php
+++ public_html/api/hilton/banners.php
@@ -4,11 +4,11 @@
 $width = $_GET['w'];
 $height = $_GET['h']; 
 $id = $_GET['id'];
-$affiliate_id = $_GET['aid'];
-$campaign_id = $_GET['cid'];
+$aid = $_GET['aid'];
+$cid = $_GET['cid'];
 $r = rand();
 ?>
 
 
-document.write('<div style="z-index: 0; position: relative;width:' + <?php echo $width;?> + 'px;height:' + <?php echo $width;?> + 'px;"><iframe width="460" height="600" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=' + $affiliate_id + '&cid=' + &campaign_id + '"></iframe></div>');
+document.write('<div style="z-index: 0; position: relative;width:' + <?php echo $width;?> + 'px;height:' + <?php echo $width;?> + 'px;"><iframe width="460" height="600" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>'"></iframe></div>');
 	
