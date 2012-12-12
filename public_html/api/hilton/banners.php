7547e60c4a7b762c2be18feaaaf612a6eb54ab68
Mike Hinchcliffe <mike@odst.co.uk>
2012-12-12 16:35:02 +0100

    iframe prob

--- public_html/api/hilton/banners.php
+++ public_html/api/hilton/banners.php
@@ -3,6 +3,20 @@
 
 $width = $_GET['w'];
 $height = $_GET['h']; 
+
+$fwidth = $_GET['w'];
+$fheight = $_GET['h']; 
+
+if( intval($fwidth) < 720 ) 
+	{
+	$fwidth = 720;
+	}
+
+if( intval($fheight) < 250 ) 
+	{
+	$fwidth = "250";
+	}
+
 $id = $_GET['id'];
 $aid = $_GET['aid'];
 $cid = $_GET['cid'];
@@ -10,5 +24,5 @@ $r = rand();
 ?>
 
 
-document.write('<div style="z-index: 0; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe width="720" height="600" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
+document.write('<div style="z-index: 0; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe width="<?php echo $fwidth;?>" height="<?php echo $fheight;?>" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
 	
