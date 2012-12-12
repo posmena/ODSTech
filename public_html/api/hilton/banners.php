3921bcedf98af8ae46aa663b907170942dcb799e
Mike Hinchcliffe <mike@odst.co.uk>
2012-12-12 17:12:38 +0100

    zindex fix

--- public_html/api/hilton/banners.php
+++ public_html/api/hilton/banners.php
@@ -3,20 +3,6 @@
 
 $width = $_GET['w'];
 $height = $_GET['h']; 
-
-$fwidth = $_GET['w'];
-$fheight = $_GET['h']; 
-
-if( intval($fwidth) < 430 ) 
-	{
-	$fwidth = "430";
-	}
-
-if( intval($fheight) < 250 ) 
-	{
-	$fheight = "250";
-	}
-
 $id = $_GET['id'];
 $aid = $_GET['aid'];
 $cid = $_GET['cid'];
@@ -24,5 +10,5 @@ $r = rand();
 ?>
 
 
-document.write('<div style="z-index: 0; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe width="<?php echo $fwidth;?>" height="<?php echo $fheight;?>" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
+document.write('<div onmouseout="this.style.zIndex = 0" onmouseover="this.style.zIndex = 2000000000"  style="z-index: 0; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe width="720" height="600" allowtransparency="true" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
 	
