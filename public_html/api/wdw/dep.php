6c0e90e38961a0095058aeb35ec2a7ec74614b45
Mike Hinchcliffe <mike@odst.co.uk>
2013-01-21 15:14:55 +0100

    add cookies to deps

--- public_html/api/wdw/dep.php
+++ public_html/api/wdw/dep.php
@@ -13,7 +13,7 @@ $newBal =  htmlentities(utf8_decode ($_GET['newbal']));
 
 $amt = str_replace('&pound;','',$newBal) - str_replace('&pound;','',$startBal);
 
-$collection->insert(array('Agent' => $_SERVER['HTTP_USER_AGENT'], 'IP' => $_SERVER['REMOTE_ADDR'], 'date' => $mongotime, 'alias' => $_GET['alias'], 'guid' => $_GET['guid'], 'amount' => $amt, 'old_balance' => $startBal, 'new_balance' => $newBal, 'site' => $_GET['site']));
+$collection->insert(array('Agent' => $_SERVER['HTTP_USER_AGENT'], 'IP' => $_SERVER['REMOTE_ADDR'], 'date' => $mongotime, 'alias' => $_GET['alias'], 'guid' => $_GET['guid'], 'amount' => $amt, 'old_balance' => $startBal, 'new_balance' => $newBal, 'site' => $_GET['site'], 'utm_source' => $_GET['utm_source'], 'utm_medium' => $_GET['utm_medium'], 'utm_term' => $_GET['utm_term'], 'utm_content' => $_GET['utm_content'], 'utm_id' => $_GET['utm_id']));
 
 header( 'Location: blank.gif' ) ;
  
