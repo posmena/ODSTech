6545a2e768a659523d643a0200091c2e767188df
Mike Hinchcliffe <mike@odst.co.uk>
2013-02-04 19:30:43 +0100

    fix

--- _private/benchukProcess.sh
+++ _private/benchukProcess.sh
@@ -9,9 +9,14 @@ php scripts/benchuk.php
 echo 'http://www.odst.co.uk/feeds/benchuk.zip?type=all' > logs/benchuk.log
 mongoexport -d odstech -c dump_bench --csv -f 'id','title','link','price','color','sizes','description','condition','shipping_cost_uk','brand','image_link','additional_image_link','category' -o files/hostedfeeds/benchuk/all.csv
 
+sed -n 's/\\"/""/g' > files/hostedfeeds/benchuk/all.csv
+mv files/hostedfeeds/benchuk/all2.csv files/hostedfeeds/benchuk/all.csv
 
 echo 'http://www.odst.co.uk/feeds/benchuk.zip?type=froogle' > logs/jtspatype.log
 mongoexport -d odstech -c dump_google_bench --csv -f 'id','item_group_id','title','link','price','sale_price','color','size','description','condition','shipping_cost_uk','brand','image_link','additional_image_link','category' -o files/hostedfeeds/benchuk/froogle.csv
+
+sed -n 's/\\"/""/g' > files/hostedfeeds/benchuk/froogle2.csv
+mv files/hostedfeeds/benchuk/froogle2.csv files/hostedfeeds/benchuk/froogle.csv
 
 ./zipFiles.sh
 
