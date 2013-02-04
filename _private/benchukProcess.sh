fac471f3c91a4eb599fdb0b4d31a73f157555569
Mike Hinchcliffe <mike@odst.co.uk>
2013-02-04 20:02:51 +0100

    fix

--- _private/benchukProcess.sh
+++ _private/benchukProcess.sh
@@ -13,7 +13,7 @@ sed 's/\\"/""/g' files/hostedfeeds/benchuk/all.csv > files/hostedfeeds/benchuk/a
 mv files/hostedfeeds/benchuk/all2.csv files/hostedfeeds/benchuk/all.csv
 
 echo 'http://www.odst.co.uk/feeds/benchuk.zip?type=froogle' > logs/jtspatype.log
-mongoexport -d odstech -c dump_google_bench --csv -f 'id','item_group_id','title','link','price','sale_price','color','size','description','condition','shipping_cost_uk','brand','image_link','additional_image_link','category' -o files/hostedfeeds/benchuk/froogle.csv
+mongoexport -d odstech -c dump_google_bench --csv -f 'id','item_group_id','title','link','price','sale_price','color','size','gender','description','condition','shipping_cost_uk','brand','image_link','additional_image_link','category' -o files/hostedfeeds/benchuk/froogle.csv
 
 sed 's/\\"/""/g' files/hostedfeeds/benchuk/froogle.csv > files/hostedfeeds/benchuk/froogle2.csv
 mv files/hostedfeeds/benchuk/froogle2.csv files/hostedfeeds/benchuk/froogle.csv
