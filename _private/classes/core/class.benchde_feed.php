bc10313db553d8a461bbdcd26df54b79ff368699
Mike Hinchcliffe <mike@odst.co.uk>
2013-02-05 14:46:27 +0100

    fix

--- _private/classes/core/class.benchde_feed.php
+++ _private/classes/core/class.benchde_feed.php
@@ -1,6 +1,6 @@
 <?php
 
-class core_benchuk_feed extends core_feed_downloader
+class core_benchde_feed extends core_feed_downloader
 {
 	public function __construct($db, $qs) {
 		parent::__construct($db, $qs, 'benchde');
