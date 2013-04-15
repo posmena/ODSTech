bf8d6a1efd0d06e88df6f6fb12f2fd5d5e7fd328
Mike Hinchcliffe <mike@odst.co.uk>
2013-04-15 19:45:33 +0200

    post

---
+++ public_html/api/hilton/post.php
@@ -0,0 +1,10 @@
+<form action='http://www3.hilton.com/en_US/hi/search/findhotels/index.htm' method='post' name='frm'>
+<?php
+foreach ($_POST as $a => $b) {
+echo "<input type='hidden' name='".htmlentities($a)."' value='".htmlentities($b)."'>";
+}
+?>
+</form>
+<script language="JavaScript">
+document.frm.submit();
+</script>
