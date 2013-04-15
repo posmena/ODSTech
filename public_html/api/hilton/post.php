7947b4e84f58ede33223276db983298423b6ede2
Mike Hinchcliffe <mike@odst.co.uk>
2013-04-15 19:49:26 +0200

    fix

--- public_html/api/hilton/post.php
+++ public_html/api/hilton/post.php
@@ -1,6 +1,6 @@
 <form action='http://www3.hilton.com/en_US/hi/search/findhotels/index.htm' method='post' name='frm'>
 <?php
-foreach ($_POST as $a => $b) {
+foreach ($_GET as $a => $b) {
 echo "<input type='hidden' name='".htmlentities($a)."' value='".htmlentities($b)."'>";
 }
 ?>
