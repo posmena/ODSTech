0c8a65c4a6d5df40b87ce0f3005ca1f852af75c5
Mike Hinchcliffe <mike@odst.co.uk>
2013-04-17 13:24:51 +0200

    disney search banner

---
+++ _private/classes/core/p20/class.disney_banners.php
@@ -0,0 +1,23 @@
+<?php
+
+class core_p20_hilton_banners extends core_default
+{
+	private $template = 'p20/disney_banners.tpl.html';
+	private $assignments;
+	
+	public function __construct($db, $qs, $ajax) {
+		
+		$affiliate_id = $qs['affiliate_id'];
+				
+		$this->assignments['p20']['affiliate_id'] = $affiliate_id;
+			
+	}
+	
+	public function getTemplate() {
+	
+		return $this->template;
+	}
+	public function getAssignments() {
+		return $this->assignments;
+	}
+}
