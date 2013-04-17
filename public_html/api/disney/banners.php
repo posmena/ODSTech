a275f523ae15453c64b352bc2621d0fbb19c45a0
Mike Hinchcliffe <mike@odst.co.uk>
2013-04-17 13:51:14 +0200

    fix

--- public_html/api/disney/banners.php
+++ public_html/api/disney/banners.php
@@ -1,9 +1,8 @@
 <?php
 $id = $_GET['id'];
 $aid = $_GET['aid'];
-$cid = $_GET['cid'];
 $r = rand();
  
 ?>
 
-document.write('<iframe height="300px" width="250px" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/disney/banner_<?php echo $id?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe>');
+document.write('<iframe height="300px" width="250px" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/disney/banner_<?php echo $id?>.php?aid=<?php echo $aid?>"></iframe>');
