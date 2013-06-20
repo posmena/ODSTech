4018d0b826d89c4cb35e2e116e235b4c2e5093e1
Mike Hinchcliffe <mike@odst.co.uk>
2013-06-21 01:12:56 +0200

    addbom

--- _private/chescaProcess.sh
+++ _private/chescaProcess.sh
@@ -9,6 +9,8 @@ php scripts/scraper.php chesca 1 1 | mail -s 'Chesca Processed' tech@odst.co.uk
 echo 'http://www.odst.co.uk/feeds/chesca.zip?type=webgains' > logs/chesca.log
 mongoexport -d odstech -c chesca_scrape --csv -f 'id','title','link','price','category','image_link','thumbnail','sizes','full_merchant_price','brand','availability','delivery_time','delivery_cost','material','description','washing_instructions' -o files/hostedfeeds/chesca/webgains.csv | mail -s "Chesca Webgains Exported" tech@odst.co.uk < logs/chesca.log
 
+php scripts/addbom.php files/hostedfeeds/chesca/webgains.csv
+
 echo 'http://www.odst.co.uk/feeds/chesca.zip?type=froogle' > logs/chesca.log
 mongoexport -d odstech -c chesca_scrape --csv -f 'id','title','link','price','condition','shipping','brand','image_link','product_type','availability','description' -o files/hostedfeeds/chesca/froogle.csv | mail -s "Chesca Froogle Exported" tech@odst.co.uk < logs/chesca.log
 
