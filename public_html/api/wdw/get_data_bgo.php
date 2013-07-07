55a365895c968045e872e73a03e292bc8df91cfb
Mike Hinchcliffe <mike@odst.co.uk>
2013-07-07 21:19:11 +0100

    wdw

---
+++ public_html/api/wdw/get_data_bgo.php
@@ -0,0 +1,47 @@
+<?php
+
+$conn = new Mongo('localhost');
+// access database
+$mdb = $conn->ubud;
+// access collection
+$collection = $mdb->ubud_tracking_dep;
+$registrations = $mdb->ubud_tracking_reg;
+$vampire_registrations = $mdb->registrations;
+
+$realtime = date("Y-m-d H:i:s");
+$mongotime = New Mongodate(strtotime($realtime));
+
+$collection->find(array('processed' => array('$exists' => false));
+
+foreach($collection as $row)
+	{
+	$row['processed'] = true;
+	$collection->save($row);
+	
+	$regs = $registrations->find(array('customer' => $row['customer'], 'guid' => array('$ne') ));
+	foreach($regs as $reg)
+		{
+		if( !isset($reg['ftd']) )
+			{
+			$reg['ftd'] = $row['date'];
+			$reg['ftds'] = 1;
+			}
+		else
+			{
+			$reg['ftds'] = $reg['ftds'] + 1;
+			$reg['last_ftd'] = $reg['date'] + 1;
+			}
+		}
+	}
+	
+	
+	$ftds = $registrations->find(array('ftds' => array('$gte' => 1 ) ) );
+	echo('<table><tr><td>Date FTD</td><td>Date Reg</td><td>GUID</td></tr>');
+	foreach($ftds as $ftd)
+		{
+		echo("<tr><td>" . $ftd['date'] . "</td><td>" . $ftd['ftd'] . "</td><td>" . $ftd['guid'] . "</td></tr>");
+		}
+	echo("</table>");
+
+ 
+?>
---
+++ public_html/api/wdw/get_data_vampire.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = new Mongo('localhost');
+// access database
+$mdb = $conn->ubud;
+// access collection
+$collection = $mdb->ubud_tracking_dep;
+$registrations = $mdb->ubud_tracking_reg;
+
+$realtime = date("Y-m-d H:i:s");
+$mongotime = New Mongodate(strtotime($realtime));
+$emails = $_POST['emails'];
+if($_POST['pwd'] != "kjsdhf898sdhnjkh")
+	{
+	die();
+	}
+
+	$ftds = $registrations->find(array('email' => array('$in' => $emails ) ) );
+	echo('<table><tr><td>Email</td><td>Date Reg</td><td>GUID</td></tr>');
+	foreach($ftds as $ftd)
+		{
+		echo("<tr><td>" . $ftd['email'] . "</td><td>" . $ftd['date'] . "</td><td>" . $ftd['utm_id'] . "</td></tr>");
+		}
+	echo("</table>");
+	
+ 
+?>
