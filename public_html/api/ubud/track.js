e9c8b7d0c02a2f892c7ece6be26019690472038b
Mike Hinchcliffe <mike@odst.co.uk>
2013-09-23 18:41:21 +0800

    fix

--- public_html/api/ubud/track.js
+++ public_html/api/ubud/track.js
@@ -1,6 +1,5 @@
 function ubud_js(src) {
-    ubud_url = '//' + src;
-    document.write('<script src="' + ubud_url + '" type="text/javascript"></scr' + 'ipt>');
+    document.write('<script src="' + src + '" type="text/javascript"></scr' + 'ipt>');
 }
 
-ubud_js('odst.co.uk/api/ubud/track.php?id=' + ubud_id  + '&site=' + ubud_site + '&type=' + ubud_conversion );
+ubud_js('https://odst.co.uk/api/ubud/track.php?id=' + ubud_id  + '&site=' + ubud_site + '&type=' + ubud_conversion );
