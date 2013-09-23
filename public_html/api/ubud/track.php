f07fcbd44b5ea5bd6ae8b59f9e0c1ec16b05d3b2
Mike Hinchcliffe <mike@odst.co.uk>
2013-09-23 18:38:09 +0800

    test

--- public_html/api/ubud/track.php
+++ public_html/api/ubud/track.php
@@ -1,4 +1,9 @@
 <?php
+if( !isset($_COOKIE['bnb_guid']) )
+	{
+	document.write("NO GUID");
+	die();
+	}
 
 $conn = new Mongo('localhost');
 // access database
@@ -20,7 +25,7 @@ $realtime = date("Y-m-d H:i:s");
 $mongotime = New Mongodate(strtotime($realtime));
 
 $guid = $_COOKIE['bnb_guid'];
-
+	
 $collection->insert(array('IP' => $_SERVER['REMOTE_ADDR'], 'date' => $mongotime, 'guid' => $guid, 'site' => $_GET['site'], 'customer' => $_GET['id']));
  
 ?>
