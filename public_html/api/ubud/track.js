3949fe47484dbf30aa71d62180a82104bc7fcadb
Mike Hinchcliffe <mike@odst.co.uk>
2013-09-23 18:59:40 +0800

    fix

--- public_html/api/ubud/track.js
+++
@@ -1,5 +0,0 @@
-function ubud_js(src) {
-    document.write('<script src="' + src + '" type="text/javascript"></scr' + 'ipt>');
-}
-
-ubud_js('https://odst.co.uk/api/ubud/track.php?id=' + ubud_id  + '&site=' + ubud_site + '&type=' + ubud_conversion );
---
+++ public_html/api/wdw/uTrack.js
@@ -0,0 +1,5 @@
+function ubud_js(src) {
+    document.write('<script src="' + src + '" type="text/javascript"></scr' + 'ipt>');
+}
+
+ubud_js('https://odst.co.uk/api/ubud/uTrack.php?id=' + ubud_id  + '&site=' + ubud_site + '&type=' + ubud_conversion );
