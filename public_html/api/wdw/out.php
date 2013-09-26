848a69a37a690a3bfd6e53cfa49075e8132a3c38
Mike Hinchcliffe <mike@odst.co.uk>
2013-09-26 23:07:59 +0800

    cookie path

--- public_html/api/wdw/out.php
+++ public_html/api/wdw/out.php
@@ -5,7 +5,8 @@
 $guid = $_GET['guid'];
 $url = $_GET['p'];
 
-setcookie("bnb_guid", $guid, time() + 86400 * 365 );
+setcookie("bnb_guid", $guid, time() + 86400 * 365, "/", "odst.co.uk" );
+setcookie("wdw_guid", $guid, time() + 86400 * 365, "/", "odst.co.uk" );
 
 header( "Location: $url" ) ;
 ?>
