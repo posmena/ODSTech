02373243e80284221ea58a2da9658f62d6b34cb6
Mike Hinchcliffe <mike@odst.co.uk>
2013-09-30 21:47:07 +0800

    ppc dta

---
+++ public_html/api/wdw/get_data_vampire_ppc.php
@@ -0,0 +1,29 @@
+<?php
+
+$conn = new Mongo('localhost');
+// access database
+$mdb = $conn->ubud;
+// access collection
+$registrations = $mdb->registrations;
+
+$realtime = date("Y-m-d H:i:s");
+$mongotime = New Mongodate(strtotime($realtime));
+$emails = explode(",", $_POST['emails']);
+
+
+if($_POST['pwd'] != "kjsdhf898sdhnjkh")
+	{
+	die();
+	}
+
+		
+	$ftds = $registrations->find(array('email' => array('$in' => $emails ) ) );
+	echo('<table><td>utm_source</td><td>utm_medium</td><td>utm_term</td><td>utm_content</td><td>match_type</td></tr>');
+	foreach($ftds as $ftd)
+		{
+		echo("<tr><td>" . $ftd['utm_source'] . "</td><td>" . $ftd['utm_medium'] . "</td><td>" . $ftd['utm_term'] . "</td><td>" . $ftd['utm_content'] . "</td><td>" . $ftd['matchtype'] . "</td></tr>");
+		}
+	echo("</table>");
+	
+ 
+?>
