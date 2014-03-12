d711a7c66b70ce8cef656f58e9f7a8ae7b65c3cc
Mike Hinchcliffe <mike@odst.co.uk>
2014-03-12 22:26:40 +0800

    check ssl

--- public_html/api/hilton/banners.php
+++ public_html/api/hilton/banners.php
@@ -1,6 +1,14 @@
 
 <?php
 
+$url "http://s.odst.co.uk";
+
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
+    || $_SERVER['SERVER_PORT'] == 443) {
+
+    $url = "https://www.odst.co.uk";
+}
+
 $width = $_GET['w'];
 $height = $_GET['h']; 
 $id = $_GET['id'];
@@ -29,4 +37,4 @@ if( $width == 120 || $width == 160)
 ?>
 
 document.write('<!--[if lt IE 9]><style>.odst_main_div {overflow:hidden;}</style><![endif]-->');
-document.write('<div class="odst_main_div" onmouseover="document.getElementById(\'odst_52652_<?php echo($r)?>\').width=<?php echo($fw)?>;document.getElementById(\'odst_52652_<?php echo($r)?>\').height=<?php echo($fh)?>" onmouseout="document.getElementById(\'odst_52652_<?php echo($r)?>\').width=\'100%\';document.getElementById(\'odst_52652_<?php echo($r)?>\').height=\'100%\'" style="z-index: 1; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe id="odst_52652_<?php echo($r)?>" allowtransparency="true" height="100%" width="100%" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="http://s.odst.co.uk/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
+document.write('<div class="odst_main_div" onmouseover="document.getElementById(\'odst_52652_<?php echo($r)?>\').width=<?php echo($fw)?>;document.getElementById(\'odst_52652_<?php echo($r)?>\').height=<?php echo($fh)?>" onmouseout="document.getElementById(\'odst_52652_<?php echo($r)?>\').width=\'100%\';document.getElementById(\'odst_52652_<?php echo($r)?>\').height=\'100%\'" style="z-index: 1; position: relative;width:<?php echo $width;?>px;height:<?php echo $height;?>px;"><iframe id="odst_52652_<?php echo($r)?>" allowtransparency="true" height="100%" width="100%" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" src="<?php echo($url)?>/api/hilton/banner_<?php echo $width?>x<?php echo $height?>.php?aid=<?php echo $aid?>&cid=<?php echo $cid?>"></iframe></div>');
